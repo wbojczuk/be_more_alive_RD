64a2352cfd21c871aad0ee334c66573f9e308ad1
William Bojczuk <williambojczuk@gmail.com>
2024-05-31 14:59:22 -0400

    updated

--- src/app/(mainsite)/components/homepage/Goals/Goals.tsx
+++ src/app/(mainsite)/components/homepage/Goals/Goals.tsx
@@ -8,31 +8,31 @@ return (
  <div className={styles.goals}>
 
     <div className={styles.goalWrapper}>
-        <Link href={"/programs"} className={styles.goal}>
+        <Link href={"/program"} className={styles.goal}>
             <h3>Life Skills Coaching</h3>
         <img src='/img/goals/lifeskills.webp' alt='Image of more men' className={`bg-img ${styles.bg}`} />
         <div className={`shader-dark ${styles.shader}`}></div>
         </Link>
 
-        <Link href={"/programs"} className={styles.goal}>
+        <Link href={"/program"} className={styles.goal}>
             <h3>Fatherhood Coaching</h3>
         <img src='/img/goals/fatherhood.webp' alt='Image of more men' className={`bg-img ${styles.bg}`} />
         <div className={`shader-dark ${styles.shader}`}></div>
         </Link>
 
-        <Link href={"/programs"} className={styles.goal}>
+        <Link href={"/program"} className={styles.goal}>
             <h3>Community Engagement</h3>
         <img src='/img/goals/community.webp' alt='Image of more men' className={`bg-img ${styles.bg}`} />
          <div className={`shader-dark ${styles.shader}`}></div>
         </Link>
 
-        <Link href={"/programs"} className={styles.goal}>
+        <Link href={"/program"} className={styles.goal}>
             <h3>Workforce Development / Training</h3>
         <img src='/img/goals/work.webp' alt='Image of more men' className={`bg-img ${styles.bg}`} />
          <div className={`shader-dark ${styles.shader}`}></div>
         </Link>
 
-        <Link href={"/programs"} className={styles.goal}>
+        <Link href={"/program"} className={styles.goal}>
             <h3>Violence Prevention</h3>
         <img src='/img/goals/violence.webp' alt='Image of more men' className={`bg-img ${styles.bg}`} />
          <div className={`shader-dark ${styles.shader}`}></div>
